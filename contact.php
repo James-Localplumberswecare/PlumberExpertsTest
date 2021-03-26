f2d745def649d86623538887c2a3d6c3de8d2035
James McGregor <james.mcgregor@localplumberswecare.co.uk>
2021-03-26 16:10:00 +0000

    contact form php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = '<website@localplumberswecare.co.uk>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = '<jmwindsurfer@googlemail.com>';
+$sendTo = '<jobs@localplumberswecare.co.uk>';
 
 // subject of the email
 $subject = 'New Enquiry from Website form';
