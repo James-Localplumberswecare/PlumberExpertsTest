ab5c89a284de3b024f0f17703eaac80997714f5a
James McGregor <james.mcgregor@localplumberswecare.co.uk>
2021-03-26 15:18:53 +0000

    contact php 3

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = '<website@localplumberswecare.co.uk>';
+$from = '<office@localplumberswecare.co.uk>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = '<jobs@localplumberswecare.co.uk>';
