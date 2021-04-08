8658f798de7652f2d24b4b29e69b76cf54914d80
James McGregor <james.mcgregor@localplumberswecare.co.uk>
2021-04-08 10:25:52 +0100

    New updates

--- contact.php
+++ contact.php
@@ -29,6 +29,12 @@ $errorMessage = 'There was an error while submitting the form. Please try again 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(-1);
 
+$recaptcha = $_POST['g-recaptcha-response'];
+$res = reCaptcha($recaptcha);
+if(!$res['success']){
+  // Error
+}
+
 try
 {
 
